cdcdfaab8486ab3d691948fda5a0946a6acb5f89
yoru <bhunjio15987@gmail.com>
2019-04-18 01:27:16 +0000

    add status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,60 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    set_task
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "Taskが正常に保存されました"
+      redirect_to @task
+    else
+      flash[:danger] = "保存に失敗しました"
+      
+      render :new
+    end
   end
 
   def edit
+    set_task
   end
 
   def update
+    set_task
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクの編集に成功しました"
+      redirect_to @task
+    else
+      flash[:danger] = "タスクの編集に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
+    set_task
+    
+    @task.destroy
+    flash[:success] = "削除されました"
+    redirect_to tasks_url
+  end
+  
+  #StrongParameter
+  def task_params
+    params.require(:task).permit(:content, :title)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190416161500) do
+ActiveRecord::Schema.define(version: 20190417090037) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.string   "status"
   end
 
 end
